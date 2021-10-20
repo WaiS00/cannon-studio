fd707262be855640329fc54c33dfda8cecc74b5d
WaiS <waisiong144@gmail.com>
2021-10-20 22:44:04 +0800

    added header.php

---
+++ header.php
@@ -0,0 +1,31 @@
+<link rel="stylesheet" href="/css/homepage.css">
+<link rel="stylesheet" href="/css/header.css">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.6.1/font/bootstrap-icons.css">
+   
+    <nav class="navbar navbar-expand-lg bg-light navbar-light py-3 fixed-top">
+        <div class="container-fluid">
+          <a href="#" class="navbar-brand">
+            <img src="resources/img/cannon_logo.png" alt="logo" width="250" height="auto" class="d-inline-block align-text-top">
+            
+        </a>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navmenu"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+
+          <div class="collapse navbar-collapse" id="navmenu">
+            <ul class="navbar-nav ms-auto">
+              <li class="nav-item">
+                <a href="#" class="nav-link"><i class="bi bi-cart" style="font-size:40px;"></i></a>
+              </li>
+              <li class="nav-item">
+                <a href="#" class="nav-link"><i class="bi bi-person-fill" style="font-size:40px;"></i></a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
