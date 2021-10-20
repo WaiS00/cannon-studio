4db8c659376fba7f83f5feb004c19607aa7ea364
WaiS <waisiong144@gmail.com>
2021-10-21 00:11:22 +0800

    Update header.php

--- header.php
+++ header.php
@@ -3,51 +3,51 @@
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.6.1/font/bootstrap-icons.css">
    
     <nav class="navbar navbar-expand-lg bg-light navbar-light py-3 fixed-top">
-        <div class="container-fluid">
-          <a href="#" class="navbar-brand">
-            <img src="resources/img/cannon_logo.png" alt="logo" width="250" height="auto" class="d-inline-block align-text-top">
-        </a>
-          <button
-            class="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navmenu"
-          >
-            <span class="navbar-toggler-icon"></span>
-          </button>
+      <div class="container-fluid">
+        <a href="#" class="navbar-brand">
+          <img src="resources/img/cannon_logo.png" alt="logo" width="250" height="auto" class="d-inline-block align-text-top">
+      </a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navmenu"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
 
-          <div class="collapse navbar-collapse" id="navmenu">
-            <ul class="navbar-nav ms-auto">
-              <li class="nav-item active">
-                <a class="nav-link active" aria-current="page" href="#">Home</a>
-              </li>
-              <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="navbarScrollingDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                  Products
-                </a>
-                <ul class="dropdown-menu" aria-labelledby="navbarScrollingDropdown">
-                  <li><a class="dropdown-item" href="#">DSLR Body</a></li>
-                  <li><a class="dropdown-item" href="#">Lenses</a></li>
-                  <li><a class="dropdown-item" href="#">Set(DSLR and Lens)</a></li>
-                  <li><a class="dropdown-item" href="#">Accessories</a></li>
-                </ul>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Services Provided</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Contact Us</a>
-              </li>
+      <div class="collapse navbar-collapse" id="navmenu">
+        <ul class="navbar-nav ms-auto">
+          <li class="nav-item active">
+            <a class="nav-link active" aria-current="page" href="#">Home</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarScrollingDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Products
+            </a>
+            <ul class="dropdown-menu" aria-labelledby="navbarScrollingDropdown">
+              <li><a class="dropdown-item" href="#">DSLR Body</a></li>
+              <li><a class="dropdown-item" href="#">Lenses</a></li>
+              <li><a class="dropdown-item" href="#">Set(DSLR and Lens)</a></li>
+              <li><a class="dropdown-item" href="#">Accessories</a></li>
             </ul>
-            <ul class="navbar-nav ms-auto">
-              <li class="nav-item">
-                <a href="#" class="nav-link"><i class="bi bi-cart" style="font-size:30px;"></i> Cart</a>
-              </li>
-              <li class="nav-item">
-                <a href="#" class="nav-link"><i class="bi bi-person-fill" style="font-size:30px;"></i>Profile</a>
-              </li>
-            </ul>
-          </div>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Services Provided</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Contact Us</a>
+          </li>
+        </ul>
+        <ul class="navbar-nav ms-auto">
+          <li class="nav-item">
+            <a href="#" class="nav-link"><i class="bi bi-cart" style="font-size:30px;"></i> Cart</a>
+          </li>
+          <li class="nav-item">
+            <a href="#" class="nav-link"><i class="bi bi-person-fill" style="font-size:30px;"></i>Profile</a>
+          </li>
+        </ul>
+      </div>
 
 
       </nav>
