debfd5e34cf7632262f8738cd55805a523c285da
WaiS <waisiong144@gmail.com>
2021-10-25 22:37:33 +0800

    fixed unused css and first draft of carousel (without description)

--- footer.php
+++ footer.php
@@ -4,37 +4,29 @@
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.6.0/font/bootstrap-icons.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
-<footer class="bg-dark text-center text-white">
+<footer class="bg-light text-center text-dark">
   <div class="container p-4 pb-0">
     <section class="mb-4">
-      <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
+      <a class="btn btn-outline-dark btn-floating m-1" href="#!" role="button"
         ><i class="fa fa-facebook-f"></i
       ></a>
 
-      <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
+      <a class="btn btn-outline-dark btn-floating m-1" href="#!" role="button"
         ><i class="fa fa-twitter"></i
       ></a>
 
-      <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
+      <a class="btn btn-outline-dark btn-floating m-1" href="#!" role="button"
         ><i class="fa fa-google"></i
       ></a>
 
-      <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
+      <a class="btn btn-outline-dark btn-floating m-1" href="#!" role="button"
         ><i class="fa fa-instagram"></i
       ></a>
 
-      <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
-        ><i class="fa fa-linkedin"></i
-      ></a>
-
-      <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
-        ><i class="fa fa-github"></i
-      ></a>
     </section>
   </div>
 
-  <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
-    © 2021 WaiS Copyright:
-    <a class="text-white" href="#">Cannon-Studio.com</a>
+  <div class="text-center p-3" style="background-color: #FCFAFA;">
+    © 2021 WaiS Copyright: <strong>Cannon-Studio Sdn Bhd</strong>   
   </div>
 </footer>
