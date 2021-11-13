e295abeaef63e811e664dc4a6e6fab49f05eaf80
WaiS <waisiong144@gmail.com>
2021-11-13 16:14:24 +0800

    found some minor bugs

--- search.php
+++ search.php
@@ -51,3 +51,40 @@ if(!empty($search)){
 ?>
 
 
+<script type="text/javascript">
+    $(document).ready(function() {
+        //https://stackoverflow.com/questions/5004233/jquery-ajax-post-example-with-php
+        // Bind to the submit event of our form
+        $(".form").submit(function(event) {
+
+            // Prevent default posting of form
+            event.preventDefault();
+
+            // setup some local variables
+            var $form = $(this);
+
+            // Let's select and cache all the fields
+            var $inputs = $form.find("input, select, button, textarea");
+
+            // Serialize the data in the form
+            var serializedData = $form.serialize();
+
+            // Let's disable the inputs for the duration of the Ajax request.
+            // Note: we disable elements AFTER the form data has been serialized.
+            // Disabled form elements will not be serialized.
+            $inputs.prop("disabled", true);
+
+            // Fire off the request to /form.php
+            request = $.ajax({
+                url: "cart_main.php",
+                type: "POST",
+                data: serializedData
+            });
+
+            // Callback handler that will be called on success
+            request.done(function(response, textStatus, jqXHR) {
+                window.alert("Item has been added to cart");
+            });
+        });
+    });
+</script>
